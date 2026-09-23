10f613df142a52e6070913cdd3ff957379ec7e41
Paul Kipkemboi <you@example.com>
2026-09-23 15:46:39 +0300

    Finish api interaction

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -1,1 +1,15 @@
+import json
+import requests
 
+class Search:
+    def Get_books(self, search_term):
+        search_term_format = search_term.replace(" ", "+")
+        URL = f'https://openlibrary.org/search.json?title={search_term_format}'
+        response = requests.get(URL).json()
+        response_formatted = f'Title: {response['docs'][0]['title']}'
+
+
+book = input('Look for a book: ')
+result = Search().Get_books(book)
+
+print(result)
